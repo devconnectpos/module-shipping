0948b97278fa755121a79a45e8426d2e74823806
Khue Quang Nguyen <khuenq@connectpos.com>
2021-10-29 18:07:15 +0700

    Release 21.10.29

--- Helper/Shipping.php
+++ Helper/Shipping.php
@@ -13,10 +13,19 @@ class Shipping
      */
     private $objectManager;
 
-    public function __construct(\SM\Integrate\Helper\Data $integrateHelper, \Magento\Framework\ObjectManagerInterface $objectManager)
-    {
+    /**
+     * @var \Magento\Shipping\Model\Config
+     */
+    protected $shipconfig;
+
+    public function __construct(
+        \SM\Integrate\Helper\Data $integrateHelper,
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        \Magento\Shipping\Model\Config $shipconfig
+    ) {
         $this->integrateHelper = $integrateHelper;
         $this->objectManager = $objectManager;
+        $this->shipconfig = $shipconfig;
     }
 
     /**
@@ -26,26 +35,11 @@ class Shipping
      */
     public function getAllowedShippingMethods()
     {
-        $allowedMethods = ['retailshipping', 'smstorepickup', 'dhl', 'ups', 'usps', 'fedex', 'flatrate', 'tablerate', 'freeshipping'];
+        $carriers = $this->shipconfig->getAllCarriers();
+        $allowedMethods = [];
 
-        if ($this->integrateHelper->isIntegrateMageShip()) {
-	        $shipperCarrier = $this->objectManager->create('Maurisource\MageShip\Model\Carrier');
-            array_push($allowedMethods, $shipperCarrier->getCarrierCode());
-        }
-
-        if ($this->integrateHelper->isIntegrateShipperHQ()) {
-            $shipperCarrier = $this->objectManager->create('ShipperHQ\Shipper\Model\Carrier\Shipper');
-            array_push($allowedMethods, $shipperCarrier->getCarrierCode());
-        }
-
-        if ($this->integrateHelper->isIntegrateMatrixRate()) {
-            $shipperCarrier = $this->objectManager->create('WebShopApps\MatrixRate\Model\Carrier\Matrixrate');
-            array_push($allowedMethods, $shipperCarrier->getCarrierCode());
-        }
-
-        if ($this->integrateHelper->isExistCustomShipping()) {
-            $shipperCarrier = $this->objectManager->create('Magecomp\Customshipping\Model\Carrier');
-            array_push($allowedMethods, $shipperCarrier->getCarrierCode());
+        foreach($carriers as $carrierCode => $carrierModel) {
+            $allowedMethods[] = $carrierCode;
         }
 
         return $allowedMethods;
